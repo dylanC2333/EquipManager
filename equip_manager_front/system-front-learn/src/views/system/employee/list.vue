0e593459e0ea6e91b1072016517f0a9e2a075737
dylanC2333 <dylanchendz@foxmail.com>
2024-08-27 23:42:52 +0800

    崔+陈，前后端，views

---
+++ equip_manager_front/system-front-learn/src/views/system/employee/list.vue
@@ -0,0 +1,298 @@
+<template>
+    <div class="app-container">
+      员工列表
+
+      <!--查询表单-->
+    <div class="search-div">
+      <el-form label-width="70px" size="small">
+        <el-row>
+          <el-col :span="24">
+            <el-form-item label="查询">
+              <el-input style="width: 100%" v-model="searchObj.keyword" placeholder="查询" @keyup.enter.native="fetchData()"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row style="display:flex">
+          <el-button type="primary" icon="el-icon-search" size="mini"  @click="fetchData()">搜索</el-button>
+          <el-button icon="el-icon-refresh" size="mini" @click="resetData">重置</el-button>
+        </el-row>
+      </el-form>
+    </div>    
+    <!-- 工具条,添加、批量删除 -->
+    <div class="tools-div">
+      <el-button type="success" icon="el-icon-plus" size="mini" @click="add">添 加</el-button>
+      <el-button class="btn-add" size = "mini" @click="batchRemove()"> 批量删除</el-button>
+    </div>
+
+    <!-- 表格 -->
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      stripe
+      border
+      style="width: 100%;margin-top: 10px;"
+      @selection-change="handleSelectionChange"
+      @sort-change="onSortChange"
+      :default-sort = "{prop: 'createTime', order:'ascending'}"
+      :sort-orders="['ascending','descending']"
+      >
+
+      <el-table-column type="selection"/>
+	  <!-- listLoading为加载组件，需要用的地方将其设为True即可 -->
+      <el-table-column
+        label="序号"
+        width="70"
+        align="center">
+        <template slot-scope="scope">
+          {{ (page - 1) * limit + scope.$index + 1 }}
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="employeeCode" label="员工编号" sortable="custom"/>
+      <el-table-column prop="employeeName" label="员工姓名" sortable="custom"/>
+      <el-table-column prop="telephone" label="电话号码" sortable="custom"/>
+      <el-table-column prop="createTime" label="创建时间" width="160" sortable="custom"/>
+      <el-table-column label="操作" width="200" align="center">
+        <template slot-scope="scope">
+
+          <el-button type="primary" icon="el-icon-edit" size="mini" @click="edit(scope.row.id)" title="修改"/>
+          <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeDataById(scope.row.id)" title="删除"/>
+
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- 分页组件 -->
+      <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="fetchData"
+      :current-page="page"
+      :page-sizes="[1, 3, 5, 10, 20, 50, 100]"
+      :page-size="limit"
+      style="padding: 30px 0; text-align: center;"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"/>
+
+        <!-- 添加弹框 -->
+        <el-dialog title="添加员工" :visible.sync="adddialogVisible" width="40%" >
+      <el-form ref="dataForm" :model="employee" label-width="150px" size="small" style="padding-right: 40px;">
+        <el-form-item label="员工名称">
+          <el-input v-model="employee.employeeName"/>
+        </el-form-item>
+        <el-form-item label="员工编码">
+          <el-input v-model="employee.employeeCode"/>
+        </el-form-item>
+        <el-form-item label="员工电话">
+          <el-input v-model="employee.telephone"/>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="adddialogVisible = false" size="small" icon="el-icon-refresh-right">取 消</el-button>
+        <el-button type="primary" icon="el-icon-check" @click="saveEmployee()" size="small">确 定</el-button>
+      </span>
+    </el-dialog>
+    
+    <!-- 修改弹框 -->
+    <el-dialog title="修改员工" :visible.sync="editdialogVisible" width="40%" >
+      <el-form ref="dataForm" :model="employee" label-width="150px" size="small" style="padding-right: 40px;">
+        <el-form-item label="员工名称">
+          <el-input v-model="employee.employeeName"/>
+        </el-form-item>
+        <el-form-item label="员工编码">
+          <el-input v-model="employee.employeeCode"/>
+        </el-form-item>
+        <el-form-item label="员工电话">
+          <el-input v-model="employee.telephone"/>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editdialogVisible = false" size="small" icon="el-icon-refresh-right">取 消</el-button>
+        <el-button type="primary" icon="el-icon-check" @click="updateEmployee()" size="small">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    </div>
+</template>
+
+<script>
+
+import api from '@/api/system/employee'
+
+export default{
+    //定义数据模型
+    data(){
+        return{
+            list:[],//角色列表
+            total:0,//总记录数
+            page:1,//当前页
+            limit:5,//每页显示记录数
+            searchObj:{},//条件查询封装对象
+            column:null,//排序字段
+            sortorder:null,//升降序条件
+
+            editdialogVisible:false,
+            adddialogVisible:false ,//弹出框
+            employee:{},//封装添加表单数据。
+            multipleSelect:[],//批量删除选中的记录列表
+        }
+    },
+
+    // 页面渲染之前获取数据
+    created(){
+        // 调用列表方法
+        this.fetchData()
+    },
+    methods:{//具体方法列表
+
+      // 每页显示记录数改变时调用
+      handleSizeChange(currentLimit){
+        this.limit = currentLimit;
+        this.fetchData();
+        //console.log(this.limit);
+      },
+
+      // 表格排序
+      onSortChange({prop,order}){
+        this.column = prop;
+        this.sortorder = order;
+        // console.log(this.column)
+        // console.log(this.sortorder)
+        this.fetchData()
+      },
+
+      //多选选项发生变化时调用
+      handleSelectionChange(selection){
+        // console.log(selection)
+        this.multipleSelect = selection
+      },
+
+      //批量删除batchRemove
+      batchRemove(){
+        if (this.multipleSelect.length === 0){
+          this.$message.warning('请选择要删除的记录！')
+          return
+        }
+        this.$confirm('此操作将永久删除选中角色, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+        }).then(()=>{
+          //确定后，调用ajax
+          //遍历selection，将id放入id列表
+          var idList = []
+          this.multipleSelect.forEach(item => {
+            idList.push(item.id)              
+          });
+          // 调用api
+          return api.batchRemove(idList)
+        }).then(response => {
+          this.fetchData()
+          this.$message.success(response.massage)
+        }).catch(error =>{
+          if (error == 'cancel'){
+            this.$message.info('取消删除')
+          }
+        })
+      },
+    
+      //修改-数据回显
+      edit(id){      
+          // 弹出框
+          this.editdialogVisible = true
+          api.getEmployeeId(id).then(response =>{
+
+            this.employee = response.data
+            console.log(this.employee)
+          });
+      },
+      //修改的方法
+      updateEmployee() {
+        api.update(this.employee)
+          .then(response =>{
+            //提示
+            this.$message({
+              type: 'success',
+              message: '修改成功!'
+            });
+            //关闭弹窗
+            this.editdialogVisible = false
+            //刷新页面
+            this.fetchData()
+          })
+      },
+      
+      //添加方法
+      saveEmployee(){
+        api.saveEmployee(this.employee)
+          .then(response =>{
+            //提示
+            this.$message({
+              type: 'success',
+              message: '添加成功!'
+            });
+            //关闭弹窗
+            this.adddialogVisible = false
+            //刷新页面
+            this.fetchData()
+          })
+      },
+
+      //点击添加，弹出框
+      add(){
+        this.adddialogVisible = true
+        this.employee = {} //保证弹出以后，表为空
+      },
+    
+      //通过id删除
+      removeDataById(id){
+        this.$confirm('此操作将永久删除该角色, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          //调用方法删除
+          api.removeId(id)
+            .then(response =>{
+              //提示
+              this.$message({
+              type: 'success',
+              message: '删除成功!'
+              });
+            })
+            //刷新
+            this.fetchData()
+        })
+      },
+
+      // 重置表单
+      resetData() {
+          console.log('重置查询表单')
+          this.searchObj = {}
+          this.column = null
+          this.sortorder = null
+          this.fetchData()
+      },
+
+      //获取数据列表
+      fetchData(pageNum=1){
+          // pageNum页数赋值
+          this.page = pageNum
+          // ajax调用后端接口
+          //console.log(this.searchObj)
+          api.getPageList(this.page,this.limit,this.searchObj,this.column,this.sortorder)
+              .then(response =>{
+                  console.log(response);
+                  console.log(this.searchObj);
+                  console.log(this.column);
+                  console.log(this.sortorder);
+                  //先用控制台输出查看参数如何对应。
+                  //每页数据列表
+                  this.list = response.data.records
+                  //总记录数
+                  this.total = response.data.total
+              })
+      }
+    }
+}
+</script>
